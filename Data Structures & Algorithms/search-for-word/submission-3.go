c5a7fd9c41afce8dea9e118d3c6cb7787ee28ca6
Tom Liu (Liu Hsiu-Chi) <79390871+Tomlord1122@users.noreply.github.com>
2026-08-08 10:24:25 +0800

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.go
@@ -0,0 +1,34 @@
+func exist(board [][]byte, word string) bool {
+	ROWS, COLS := len(board), len(board[0])
+	dirs := [][]int{{0,1}, {1,0}, {-1,0}, {0,-1}}
+	visited := make(map[[2]int]bool)
+
+	var dfs func(r, c, i int) bool
+	dfs = func(r, c, i int) bool{
+		if i == len(word){
+			return true
+		}
+		if min(r, c) < 0 || r == ROWS || c == COLS || 
+		visited[[2]int{r, c}] || board[r][c] != word[i]{
+			return false
+		}
+		visited[[2]int{r, c}] = true
+		res := false
+		for _, d := range dirs{
+			res = res || dfs(r+d[0], c+d[1], i+1)
+		}
+		visited[[2]int{r, c}] = false
+		return res
+	}
+
+
+
+	for r := 0; r < ROWS; r++{
+		for c := 0; c < COLS; c++{
+			if dfs(r, c, 0){
+				return true
+			}
+		}
+	}
+	return false
+}
